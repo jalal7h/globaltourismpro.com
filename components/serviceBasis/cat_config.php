52be72164086ce39f02df328c29e3ea72b23c43b
Meysam Taghipoor <taghipoor.meysam@gmail.com>
2016-11-18 13:24:57 +0330

    serviceBasis

---
+++ components/serviceBasis/cat_config.php
@@ -0,0 +1,17 @@
+<?
+
+# jalal7h@gmail.com
+# 2016/11/18
+# 1.0
+
+$GLOBALS['cat_items']['serviceBasis'] = [
+	'name'		=> 'service Basis' ,
+	'dashboard'	=> '05a' ,
+	'sub'		=> false ,
+	'icon'		=> true ,
+	'desc'		=> true ,
+	'flag'		=> true ,
+	'ccf'		=> false ,
+	'kw'		=> false ,
+	'color'		=> false ,
+];
---
+++ components/serviceBasis/serviceBasis_option.php
@@ -0,0 +1,11 @@
+<?
+
+# jalal7h@gmail.com
+# 2016/11/18
+# 1.0
+
+
+function serviceBasis_option()
+{
+	return cat_display('serviceBasis');
+}
