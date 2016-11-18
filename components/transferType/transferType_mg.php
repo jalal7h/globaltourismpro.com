134107fed1a0b99452ceddb49d1606060c823543
Meysam Taghipoor <taghipoor.meysam@gmail.com>
2016-11-18 11:14:29 +0330

    transferType_mg

--- components/transferType/transferType_mg.php
+++ components/transferType/transferType_mg.php
@@ -3,5 +3,15 @@
 # jalal7h@gmail.com
 # 2016/11/17
 # 1.0
-
-
+$GLOBALS['cmp-icon']['transferType'] = '072';
+$GLOBALS['cat_items']['transferType'] = [
+	'name'		=> 'transferType' ,
+	'dashboard'	=> true ,
+	'sub'		=> false ,
+	'icon'		=> true ,
+	'desc'		=> true ,
+	'flag'		=> true ,
+	'ccf'		=> false ,
+	'kw'		=> false ,
+	'color'		=> false ,
+];
