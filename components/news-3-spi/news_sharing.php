e9dd28a9f53e5748c6866688930a57d1ae97bf69
Meysam Taghipoor <taghipoor.meysam@gmail.com>
2016-11-25 16:30:23 +0330

    edit-news-3

--- components/news-3-spi/news_sharing.php
+++ components/news-3-spi/news_sharing.php
@@ -4,7 +4,7 @@ function news_sharing( $rw ){
 	
 	$link = news_link($rw);
     
-	$socials = seo_share_link( $link , $rw['name'] );
+	$socials = news_share_link( $link , $rw['name'] );
 	
 	if( $socials ){
 		$socials = '
@@ -24,3 +24,17 @@ function news_sharing( $rw ){
 	return $c;
 }
 
+
+function news_share_link( $link , $title="" ){
+
+	$c.= "
+	<div class='seo_share_link'>
+		<a href='http://www.facebook.com/sharer/sharer.php?u=".urlencode($link)."' target='_blank' class='facebook' ><icon></icon></a>
+		<a href='http://twitter.com/share?url=".$link."' target='_blank' class='twitter' ><icon></icon></a>
+		<a href='https://pinterest.com/pin/create/button/?url=&media=".urlencode($link)."&description=".urlencode($title)."' target='_blank' class='pinterest' ><icon></icon></a>
+		<a href='https://www.linkedin.com/shareArticle?mini=true&url=".urlencode($link)."&title=".urlencode($title)."&summary=&source=' target='_blank' class='linkedin' ><icon></icon></a>
+	</div>
+	";
+	
+	return $c;
+}
