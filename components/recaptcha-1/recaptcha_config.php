bf4c1d466d4d2cfe7f5b43322eb1e629c5e32d7e
Jalal Hoseini <jalal7h@gmail.com>
2017-05-11 13:07:16 +0430

    .

--- components/recaptcha-1/recaptcha_config.php
+++ components/recaptcha-1/recaptcha_config.php
@@ -1,12 +1,12 @@
 <?php
 
 # jalal7h@gmail.com
-# 2017/02/11
+# 2017/05/11
 # 1.1
 
 # --spi--
 
-define( 'recaptcha_public_key' , '6LdcwCAUAAAAAAaM3SY5aOvQi_4asB9aHSbma2eg' );
-define( 'recaptcha_secred_key' , '6LdcwCAUAAAAADRkSESdpGhg89Q__uqc8xHcAm0o' );
+define( 'recaptcha_public_key' , '6LcC7SAUAAAAAEttuK0ZLo3M2qtGRB1qIdt46Rit' );
+define( 'recaptcha_secred_key' , '6LcC7SAUAAAAANZY8RxgiCzIGeBHuiHq2QQp1KV6' );
 
 
