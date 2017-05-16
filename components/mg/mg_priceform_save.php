13207d78a69f722f7bf3355a9936ab029afd7f34
Jalal Hoseini <jalal7h@gmail.com>
2017-05-16 13:16:38 +0430

    .

--- components/mg/mg_priceform_save.php
+++ components/mg/mg_priceform_save.php
@@ -14,18 +14,25 @@ function mg_priceform_save( $table ){
 
 		$cat_id = substr( $key, 6 );
 
-		foreach( $_REQUEST[ 'price_'.$cat_id ] as $i => $price ){
-			if(! $price ){
-				continue;
-			} else {
-				$more_than = $_REQUEST[ 'more_than_'.$cat_id ][ $i ];
-				$set[ $cat_id ][ $more_than ] = $price;
+		if( sizeof($_REQUEST[ 'price_'.$cat_id ]) ){
+			foreach( $_REQUEST[ 'price_'.$cat_id ] as $i => $price ){
+				if(! $price ){
+					continue;
+				} else {
+					$more_than = $_REQUEST[ 'more_than_'.$cat_id ][ $i ];
+					$set[ $cat_id ][ $more_than ] = $price;
+				}
 			}
 		}
 
 	}
 
-	mg_pricelist_set( $table, $set );
+	if(! sizeof($set) ){
+		echo convbox_back( 'We need you to put at lease one price for this product.' , 'red' );
+
+	} else {
+		mg_pricelist_set( $table, $set );
+	}
 
 }
 
