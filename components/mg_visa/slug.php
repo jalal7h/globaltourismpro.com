e9960aed68779e4c3845264b373db83b49fa36a3
Jalal Hoseini <jalal7h@gmail.com>
2017-05-28 23:46:27 +0430

    .

--- components/mg_visa/slug.php
+++ components/mg_visa/slug.php
@@ -4,5 +4,10 @@
 # 2017/05/25
 # 1.0
 
-add_slug( Slug::getSlugByName('userpanel').'/visa/$', _URL.'/?page=14&do_slug=visa&do=form&id=$1' );
+add_slug([
+
+	Slug::getSlugByName('userpanel').'/visa/new' => _URL.'/?page=14&do_slug=visa&do=form',
+	Slug::getSlugByName('userpanel').'/visa/$' => _URL.'/?page=14&do_slug=visa&do=form&id=$1',
+
+]);
 
