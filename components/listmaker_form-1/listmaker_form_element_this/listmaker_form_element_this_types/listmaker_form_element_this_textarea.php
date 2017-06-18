a4aae0984c32edf4ddc501555844fba59c4721ab
Jalal Hoseini <jalal7h@gmail.com>
2017-06-19 00:17:19 +0430

    .

--- components/listmaker_form-1/listmaker_form_element_this/listmaker_form_element_this_types/listmaker_form_element_this_textarea.php
+++ components/listmaker_form-1/listmaker_form_element_this/listmaker_form_element_this_types/listmaker_form_element_this_textarea.php
@@ -12,9 +12,12 @@ function listmaker_form_element_this_textarea( $info ){
 	#
 	# tinymce
 	if( strstr( " ".strtolower($info['class'])." ", ' tinymce ') ){
-		$c = '<script src="http://cdn.tinymce.com/4/tinymce.min.js"></script>'.
-			 '<script src="'._URL.'/modules/tinymce/tinymce-set.js"></script>'.
-			 $c;
+		if(! $GLOBALS['listmaker_form_element_this_textarea_include_done'] ){
+			$c = "<script src=\""._URL."/modules/tinymce/tinymce.min.js\"></script>\n".
+				 "<script src=\""._URL."/modules/tinymce/tinymce-set.js\"></script>\n".
+				 $c;
+			$GLOBALS['listmaker_form_element_this_textarea_include_done'] = true;
+		}
 	}
 
 	$id = $info['id'] ? $info['id'] : "lmfe_".$info['formName']."_".$info['name'];
